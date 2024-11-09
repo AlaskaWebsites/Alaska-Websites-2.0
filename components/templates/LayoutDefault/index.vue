c5c7438d3d63e3ecde979a40e5c59fff81dfdbdf
unknown <alaskawebsites94@gmail.com>
2024-11-09 17:26:42 -0300

    Subindo att whatspp button

--- components/templates/LayoutDefault/index.vue
+++ components/templates/LayoutDefault/index.vue
@@ -2,7 +2,11 @@
   <Nav />
   <slot />
   <Footer />
-  <button><i class="fa-brands fa-square-whatsapp icon-whatsapp"></i></button>
+  <a
+    href="https://wa.me/5511969124940?text=Ol%C3%A1%21+Venho+atrav%C3%A9s+do+seu+site%2C+gostaria+de+saber+mais+sobre+seus+servi%C3%A7os."
+    target="_blank"
+    ><i class="fa-brands fa-square-whatsapp icon-whatsapp"></i
+  ></a>
 </template>
 <script setup>
 import Nav from "./Partials/Nav/index";
