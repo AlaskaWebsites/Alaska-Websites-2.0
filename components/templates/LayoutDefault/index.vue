6f6bec6827696db6c74943f06e9a2239227163ab
unknown <alaskawebsites94@gmail.com>
2024-11-13 00:08:08 -0300

    Subindo ajuste ssr pinia

--- components/templates/LayoutDefault/index.vue
+++ components/templates/LayoutDefault/index.vue
@@ -1,44 +1,42 @@
 <template>
-  <ClientOnly>
-    <Nav />
-    <slot />
-    <Footer />
-    <a
-      href="https://wa.me/5511969124940?text=Ol%C3%A1%21+Venho+atrav%C3%A9s+do+seu+site%2C+gostaria+de+saber+mais+sobre+seus+servi%C3%A7os."
-      target="_blank"
-      ><i class="fa-brands fa-square-whatsapp icon-whatsapp"></i
-    ></a>
-    <div class="controllers">
-      <button
-        @click="changeModeTemplate(!modeTemplateState)"
-        :class="`controller-mode-template ${defineControllerBackgroundTemplate}`"
-      >
-        <div>
-          <i
-            :class="`controller-mode-template-icon-primary ${
-              !modeTemplateState ? 'fa-solid fa-sun ' : 'fa-solid fa-moon'
-            }`"
-          ></i>
-        </div>
-        <div>
-          <i
-            :class="`fa-solid fa-chevron-right ${
-              !modeTemplateState
-                ? 'controller-arrow-icon-template-primary'
-                : 'controller-arrow-icon-template-secondary'
-            }`"
-          ></i>
-        </div>
-        <div>
-          <i
-            :class="`controller-mode-template-icon-secondary ${
-              !modeTemplateState ? 'fa-solid fa-moon' : 'fa-solid fa-sun'
-            } `"
-          ></i>
-        </div>
-      </button>
-    </div>
-  </ClientOnly>
+  <Nav />
+  <slot />
+  <Footer />
+  <a
+    href="https://wa.me/5511969124940?text=Ol%C3%A1%21+Venho+atrav%C3%A9s+do+seu+site%2C+gostaria+de+saber+mais+sobre+seus+servi%C3%A7os."
+    target="_blank"
+    ><i class="fa-brands fa-square-whatsapp icon-whatsapp"></i
+  ></a>
+  <div class="controllers">
+    <button
+      @click="changeModeTemplate(!modeTemplateState)"
+      :class="`controller-mode-template ${defineControllerBackgroundTemplate}`"
+    >
+      <div>
+        <i
+          :class="`controller-mode-template-icon-primary ${
+            !modeTemplateState ? 'fa-solid fa-sun ' : 'fa-solid fa-moon'
+          }`"
+        ></i>
+      </div>
+      <div>
+        <i
+          :class="`fa-solid fa-chevron-right ${
+            !modeTemplateState
+              ? 'controller-arrow-icon-template-primary'
+              : 'controller-arrow-icon-template-secondary'
+          }`"
+        ></i>
+      </div>
+      <div>
+        <i
+          :class="`controller-mode-template-icon-secondary ${
+            !modeTemplateState ? 'fa-solid fa-moon' : 'fa-solid fa-sun'
+          } `"
+        ></i>
+      </div>
+    </button>
+  </div>
 </template>
 <script setup>
 import Nav from "./Partials/Nav/index";
